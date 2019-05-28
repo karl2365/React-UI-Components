28ec2c6a6e5a3170253f54c37b81a281813264c7
Karl Beksa <karlbeksa@gmail.com>
2019-05-28 10:53:01 -0400

    MVP Complete

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Card.css';
+
+function CardContent() {
+    return(
+        <div>
+            <p class='card-content'><strong>Get started with React</strong></p>
+            <p class='card-content'>React makes it painless to create interactive UIs. Design simple views for each state in you application.</p>
+            <p class="lighter card-content">reactjs.org</p>
+        </div>
+    );
+}
+
+export default CardContent;
