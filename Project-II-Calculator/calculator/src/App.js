7a74811a8bc7bb8160cfa78fe55ef944b866028d
Karl Beksa <karlbeksa@gmail.com>
2019-05-28 16:52:03 -0400

    calculator DRY-er

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import Display from './components/DisplayComponents/Display';
 import Number from "./components/ButtonComponents/Number";
-import Operation from "./components/ButtonComponents/Operation";
 import Action from "./components/ButtonComponents/Action";
 
 const App = () => {
@@ -10,21 +9,21 @@ const App = () => {
     <div className='container'>
       <Display />
       <Action action="clear" actionStyle="clear" />
-      <Operation operation='&divide;' buttonStyle='operation'  />
+      <Number number='&divide;' buttonStyle='operation'  />
       <Number number='7'  buttonStyle='number'  />
       <Number number='8' buttonStyle='number'  />
       <Number number='9' buttonStyle='number'  />
-      <Operation operation='X' buttonStyle='operation' />
+      <Number number='X' buttonStyle='operation' />
       <Number number='4' buttonStyle='number'  />
       <Number number='5' buttonStyle='number'  />
       <Number number='6' buttonStyle='number'  />
-      <Operation operation='-'  buttonStyle='operation' />
+      <Number number='-'  buttonStyle='operation' />
       <Number number='1' buttonStyle='number'  />
       <Number number='2' buttonStyle='number'  />
       <Number number='3' buttonStyle='number'  />
-      <Operation operation='+' buttonStyle='operation'  />
+      <Number number='+' buttonStyle='operation'  />
       <Action action="0" actionStyle="zero" />
-      <Operation operation='=' buttonStyle='operation'  />
+      <Number number='=' buttonStyle='operation'  />
 
     </div>
   );
